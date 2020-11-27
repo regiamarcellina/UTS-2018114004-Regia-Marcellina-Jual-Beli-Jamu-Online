96d8fb06efda51f0b768fcd4d3eb6f47f2df75f7
regiamarcellina <regiamarcelina@gmail.com>
2020-11-27 13:23:25 +0700

    commit uts

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>@yield('title')</title>
+  </head>
+  <body>
+  <div class="p-3 mb-2 bg-warning text-dark">
+      <h1 align="center">ToeTje</h1>
+        <h5 align="center">Jamu Seger</h5>    
+      <table align="center">
+          <tr>
+              <td> 
+  <div class="card" style="width: 18rem;">
+  
+  <img class="card-img-top" src="/resources/images/1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Lemon Sereh</h5>
+    <p class="card-text"><b>Komposisi:</b> Air, Sereh, Jeruk Lemon, Gula Batu, Selasih<br>
+                        <br><b>Khasiat</b>: Menyehatkan saluran cerna, menurunkan berat badan, menyehatkan kulit, mencegah kerusakan gigi, menstabilkan tekanan darah, mencegah rematik.</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div> </td>
+
+<td> <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Kunyit Asem</h5>
+    <p class="card-text"><b>Komposisi</b>: Air, Empu Kunyit, Asem Jawa, Gula Aren, Sereh, Sejumput garam<br><br><b>Khasiat</b>: Meredakan nyeri haid, sebagai antioksidan, melancarkan pencernaan, menangkal radikal bebas, menurunkan berat badan, mengendalikan gula darah.</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</div> </td>
+<td> <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Beras Kencur</h5>
+    <p class="card-text"><b>Komposisi</b>: Air, Tepung Beras, Gula Aren, Kencur, Jahe Emprit, Cengkeh, Kapulaga, Kayu Manis, Kedawung, Jeruk Nipis, Garam Sejumput.<br><br><b>Khasiat</b>: Meningkatkan nafsu makan, mencegah dan mengobati jerawat pada wajah, menangkal radikal bebas dan polusi udara, dan mengobati rasa sakit serta pegal-pegal, meredakan diare, meredakan batuk berdahak, mempertahankan gula darah.</p>
+    <a href="#" class="btn btn-primary">Pesan</a>
+  </div>
+</td>
+          </tr>
+
+      </table>
+  
+
+</span>
+</div>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
